b8c50f5988d6a8d0fbceb5b48ddcd5a3d39746f4
Saad Khattak <saadrustam@gmail.com>
2017-01-30 00:49:39 -0500

    CMakeLists now generates a compile_commands.json file for use with YouCompleteMe on Linux

--- engine/src/include/tloc_window.cpp
+++ engine/src/include/tloc_window.cpp
@@ -1,8 +1,9 @@
 #include "tloc_window.h"
 
 #include <GLFW/glfw3.h>
+#include <string>
 
 namespace tloc
 {
-  GLFWwindow* window;
+    GLFWwindow* window;
 };
--- engine/src/include/tloc_window.h
+++ engine/src/include/tloc_window.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <vector>
+#include <GLFW/glfw3.h>
+
 namespace tloc
 {
+    class MyClass
+    {
+        public:
+            std::vector<int> m;
+    };
 };

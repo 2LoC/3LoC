772501070db9efc229e00857d9c3e49448b21b1c
Saad Khattak <saadrustam@gmail.com>
2017-10-04 18:30:48 -0400

    Setup no longer requires root

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-if [ "$EUID" -ne 0 ]
-then echo "Please run as root"
-  exit
-fi
 
-apt-get install libx11-dev -y
-apt-get install xorg-dev libglu1-mesa-dev -y
+# get password from user to get uninterrupted setup
+sudo true || exit 1
+
+sudo apt-get install libx11-dev -y
+sudo apt-get install xorg-dev libglu1-mesa-dev -y
+
+mkdir -p build
+cd build
+cmake ../
+cmake --build .
+
+cd ..
+cd engine
 
 mkdir -p build
 cd build
